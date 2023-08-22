34e23acfdc2e65e3926ab762ed23057f964a7379
unknown <atharv.agarwal645@gmail.com>
2023-08-22 18:29:34 +0530

    modified proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
-    proxy:"https://image-compression-l7vd.vercel.app"
+    proxy:{
+      '/':"https://image-compression-l7vd.vercel.app/"
+    }
   },
   plugins: [react()],
 })
