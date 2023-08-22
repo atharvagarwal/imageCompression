d67f31994b8e773d265b8c550f071e7b3f126791
unknown <atharv.agarwal645@gmail.com>
2023-08-22 19:02:41 +0530

    modified cors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import { createProxy } from 'vite-plugin-proxy';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [createProxy({
-    // Proxy configuration
-    '/api': {
-      target: 'https://image-compression-l7vd.vercel.app',
-      changeOrigin: true,
-      secure: false, // If the target is an HTTPS URL
-      rewrite: (path) => path.replace(/^\/api/, ''),
-    },
-  })],
-});
-
+  plugins: [react()],
+})
